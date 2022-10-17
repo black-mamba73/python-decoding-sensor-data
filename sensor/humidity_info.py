2e3dd005b0cf7757c397e6391504d486602aaf47
black-mamba73 <j.n.orihito.111@gmail.com>
2022-10-18 06:34:27 +1000

    Update humidity_info.py

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -4,9 +4,9 @@ from datetime import date
 class HumidityData(HouseInfo):
     def _convert_data(self, data):
         recs = []
-
         for rec in data:
-            recs.append(float(rec, *100))
+            recs.append(float(rec) * 100))
+        return recs
 
     def get_data_by_area(self, rec_area=0):
         recs = super().get_data_by_area("humidity", rec_area)
